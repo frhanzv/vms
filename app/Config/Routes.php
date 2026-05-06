6aec699d3e308b24044b7b479a21f3e4f7389824
Tan Jia Yee <tanjy-wm24@student.tarc.edu.my>
2026-05-06 14:14:50 +0800

    qr

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -272,6 +272,7 @@ $routes->group('', ['filter' => $plusHost], function($routes) {
     $routes->post('visitors/update', 'VisitorList::updateVisitor');
     $routes->post('visitors/bindCard', 'VisitorList::bindCard');
     $routes->post('visitors/unbindCard', 'VisitorList::unbindCard');
+    $routes->get('visitors/generateQr/(:num)', 'VisitorList::generateQr/$1');
 });
 
 // ===========================
