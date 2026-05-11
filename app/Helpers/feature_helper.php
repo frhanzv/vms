ba331275298849b761ac244bcc60a17caa5382bc
dzuliskndr <122595025+dzuliskndr@users.noreply.github.com>
2026-05-11 16:25:46 +0800

    GXO Credentials Bug Fix #3

---
+++ app/Helpers/feature_helper.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!function_exists('client_feature_enabled')) {
+    function client_feature_enabled(string $featureKey): bool
+    {
+        if (session()->get('role') === 'superadmin') {
+            return true;
+        }
+
+        $companyId = (int) (
+            session()->get('company_id')
+            ?: ((new \App\Models\UserModel())->find((int) session()->get('user_id'))['company_id'] ?? 0)
+        );
+
+        return (new \App\Models\ClientFeatureModel())->isEnabled($companyId, $featureKey);
+    }
+}
